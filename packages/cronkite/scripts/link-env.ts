a3c5d00c1885b128b72528d0521f6e8781a5ce4b
cdaringe <christopher.dieringer@gmail.com>
2019-06-09 18:13:15 -0700

    feat: link .env file

---
+++ packages/cronkite/scripts/link-env.ts
@@ -0,0 +1,16 @@
+import { link } from 'fs'
+import path from 'path'
+const SRC_ENV_FILENAME = path.resolve(
+  __dirname,
+  '..',
+  '..',
+  '..',
+  'informed.env'
+)
+const TARGET_ENV_FILENAME = path.resolve(__dirname, '..', '.env')
+
+link(SRC_ENV_FILENAME, TARGET_ENV_FILENAME, err => {
+  const isEnvFilePreexisting = err && err.code && err.code.match(/exist/i)
+  if (err && !isEnvFilePreexisting) throw err
+  console.log('.env file ok')
+})
